b4a034cf345987f1665d0ad8cc1354f5a0af8abb
cevoj35 <cevoj35548@teamaurous.com>
2023-04-07 01:40:52 +0000

    Remove --onefile build arg due to high AV false positives

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 :: Build script for compiled exe using nuitka
 
 @prompt Executing:  
-python -m nuitka --standalone --onefile --remove-output --assume-yes-for-downloads --disable-console --clean-cache=all --enable-plugin=pyqt5 --include-data-files=MainWindow.ui=MainWindow.ui --include-data-dir=assets=assets "--windows-icon-from-ico=%~dp0assets\icon.ico" FmhyChecker.pyw
+python -m nuitka --standalone --remove-output --assume-yes-for-downloads --disable-console --clean-cache=all --enable-plugin=pyqt5 --include-data-files=MainWindow.ui=MainWindow.ui --include-data-dir=assets=assets "--windows-icon-from-ico=%~dp0assets\icon.ico" FmhyChecker.pyw
 
 @echo off
 if %errorlevel%==0 (
